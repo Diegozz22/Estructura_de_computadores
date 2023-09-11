7625447a9bd548ea0d7bc87aace91dfba9010657
Diegozz22 <129340688+Diegozz22@users.noreply.github.com>
2023-09-10 22:22:40 -0500

    Trabajo

---
+++ Numeromenor.asm
@@ -0,0 +1,96 @@
+.data
+    pra1: .asciiz "Ingresa los números para encontrar el menor. \n"
+    pra2: .asciiz "Ingresa el primer número: "
+    pra3: .asciiz "Ingresa el segundo número: "
+    pra4: .asciiz "Ingresa el tercer número: "
+    pra5: .asciiz "El número menor es: "
+    pra6: .asciiz "\n Existen números iguales. \n"
+    pra7: .asciiz "Ingresa números diferentes. \n"
+    
+    lineN: .asciiz "\n"  # Salto de línea
+
+.text 
+.globl main
+
+main:
+    # Muestra el mensaje de entrada pra1
+    li $v0, 4
+    la $a0, pra1
+    syscall
+
+    loop:
+        # Muestra el mensaje de entrada pra2
+        li $v0, 4
+        la $a0, pra2
+        syscall  
+
+        # Lee el primer número entero
+        li $v0, 5
+        syscall
+        move $t0, $v0  # Almacena el primer número en $t0
+
+        # Muestra el mensaje de entrada pra3
+        li $v0, 4
+        la $a0, pra3
+        syscall
+
+        # Lee el segundo número entero
+        li $v0, 5
+        syscall
+        move $t1, $v0
+        
+        # Muestra el mensaje de entrada pra4
+        li $v0, 4
+        la $a0, pra4
+        syscall
+        
+        # Lee el tercer número entero
+        li $v0, 5
+        syscall
+        move $t2, $v0
+
+        # Compara los tres números para encontrar si son iguales
+        beq $t0, $t1, SonIguales
+        beq $t0, $t2, SonIguales
+        beq $t1, $t2, SonIguales
+
+        # Encuentra el número mayor
+        move $t3, $t0
+        blt $t1, $t3, MenorT1
+        blt $t2, $t3, MenorT2
+        j fin
+
+    MenorT1:
+        move $t3, $t1
+        blt $t2, $t3, MenorT2
+        j fin
+
+    MenorT2:
+        move $t3, $t2
+        j fin
+
+    SonIguales:
+        # Imprime el mensaje para números iguales
+        li $v0, 4
+        la $a0, pra6
+        syscall
+        
+        li $v0, 4
+        la $a0, pra7
+        syscall
+        j loop
+
+    fin:
+        # Imprime el mensaje de respuesta pra5
+        li $v0, 4
+        la $a0, pra5
+        syscall  
+
+        # Imprime el número mayor
+        li $v0, 1
+        move $a0, $t3
+        syscall
+
+        # Termina el programa
+        li $v0, 10
+        syscall
---
+++ Serie_Fibona.asm
@@ -0,0 +1,51 @@
+.data
+newline: .asciiz "\n"
+result: .asciiz "Serie Fibonacci: \n"
+Ques: .ascii "Cuantos quieres que imprima la serie?: "
+
+.text
+.globl main
+
+main:
+    # Inicializar variables
+    li $t0, 0         # Número actual
+    li $t1, 1         # Siguiente número
+    
+    # Muestra el mensaje Ques
+    li $v0, 4	
+    la $a0, Ques      
+    syscall 
+    
+    # Se ingresa hasta que liminte quiere que imprima la serie
+    li $v0, 5
+    syscall
+    move $t2, $v0        # Límite de la serie (cambia según necesites)
+
+    # Imprimir mensaje inicial
+    li $v0, 4
+    la $a0, result
+    syscall
+
+loop:
+    # Imprimir el número actual
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    # Imprimir un salto de línea
+    li $v0, 4
+    la $a0, newline
+    syscall
+
+    # Calcular el siguiente número de Fibonacci
+    add $t3, $t0, $t1   # $t3 = $t0 + $t1
+    move $t0, $t1       # $t0 = $t1
+    move $t1, $t3       # $t1 = $t3
+
+    # Decrementar el contador y verificar si alcanzamos el límite
+    subi $t2, $t2, 1
+    bnez $t2, loop
+
+    # Terminar el programa
+    li $v0, 10
+    syscall
